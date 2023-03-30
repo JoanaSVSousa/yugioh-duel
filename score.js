5661aa999e3a382c42499ffbab39db2ed172e114
JoanaSVSousa <105821815+JoanaSVSousa@users.noreply.github.com>
2023-03-30 15:13:33 +0100

    Update score.js

--- score.js
+++ score.js
@@ -1,26 +1,24 @@
+// Setting up the game and conditionals
 const resetButton = document.querySelector("#reset");
 const playSelector = document.querySelector("#playto");
-
+let damage = document.querySelector("#damage");
+let isGameOver = false;
+// Setting up the players
 const p1 = {
     score: document.querySelector("#playto").value,
     button: document.querySelector("#p1Button"),
     display: document.querySelector("#p1Display")
 }
-
 const p2 = {
     score: document.querySelector("#playto").value,
     button: document.querySelector("#p2Button"),
     display: document.querySelector("#p2Display")
 }
-
-let damage = document.querySelector("#damage");
-let winScore = 0;
-let isGameOver = false;
-
+// Score Calculator
 function updateScore(player, opponent) {
     if (!isGameOver) {
-        player.display.textContent = player.score;
         player.score -= parseInt(damage.value);
+        player.display.textContent = player.score;
         if (player.score <= 0) {
             isGameOver = true;
             player.display.classList.add("has-text-danger");
@@ -34,15 +32,12 @@ function updateScore(player, opponent) {
 p1.button.addEventListener("click", function () {
     updateScore(p1, p2);
 })
-
 p2.button.addEventListener("click", function () {
     updateScore(p2, p1);
 })
-
+// Setting up New Duel
 playSelector.addEventListener("change", reset)
-
 resetButton.addEventListener("click", reset)
-
 function reset() {
     isGameOver = false;
     for (let p of [p1, p2]) {
